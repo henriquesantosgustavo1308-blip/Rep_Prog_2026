022171bbc0bee70c737811b9bd418cd9f7a39814
henriquesantosgustavo1308-blip <henrique.santos.gustavo1308@escola.pr.gov.br>
2026-09-01 10:27:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarMensagem(problema) {
+
+```
+alert(
+    "🎮 DESAFIO DO GAME DO SABER\n\n" +
+    "Problema: " + problema + "\n\n" +
+    "Agora pense: o que nós podemos fazer para melhorar essa situação?"
+);
+```
+
+}
